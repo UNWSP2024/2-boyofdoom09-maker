cc4230d3f970db678b966c310dd8ecfab93235be
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-09-12 16:28:53 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -3,5 +3,8 @@ def personal_information():
     print('2009 Bacon St. Albert Lea MN')
     print('+1-452-555-6543')
     print('Undeclared')
+    #Elijah Quigley 
+    #9/12/2025
+    #Personal Information
 # Line which calls the above function.
 personal_information()
