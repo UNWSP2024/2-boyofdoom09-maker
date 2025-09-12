b1ec67fccdd48558c82b95954b04869c14cbf6cf
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-09-12 15:02:56 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,4 +1,9 @@
 def calculate_total_purchase():
+    item1 = input(first item price)
+    item2 = input(second item price)
+    item3 = input(third item price)
+    item4 = input(fourth item price)
+    item5 = input(fifth item price)
     # A customer in a store is purchasing five items.  
     # Write a program that asks for each item, 
     # then displays the subtotal of the sale, 
