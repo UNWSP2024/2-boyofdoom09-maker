77b8a665dacdda63265c536f441f0c2243644d51
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-09-12 15:04:16 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -6,6 +6,11 @@ def average_age():
     age3 = input('Enter third age')
     age4 = input('Enter fourth age')
     age5 = input('Enter fifth age')
+    age1 = int(age1)
+    age2 = int(age2)
+    age3 = int(age3)
+    age4 = int(age4)
+    age5 = int(age5)
     average_age = (age1 + age2 + age3 + age4 + age5) / 5
     print(average)
              
