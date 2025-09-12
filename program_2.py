d866bec08167fc561994d129c4e33c5616a5d7e8
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-09-12 16:10:43 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,10 +1,10 @@
 
 def average_age():
   age1 = int(input("Enter the age of friend 1: "))
-age2 = int(input("Enter the age of friend 2: "))
-age3 = int(input("Enter the age of friend 3: "))
-age4 = int(input("Enter the age of friend 4: "))
-age5 = int(input("Enter the age of friend 5: "))
+   age2 = int(input("Enter the age of friend 2: "))
+    age3 = int(input("Enter the age of friend 3: "))
+     age4 = int(input("Enter the age of friend 4: "))
+      age5 = int(input("Enter the age of friend 5: "))
 
 
 average_age = (age1 + age2 + age3 + age4 + age5) / 5.0
